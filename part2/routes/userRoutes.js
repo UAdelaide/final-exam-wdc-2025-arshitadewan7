94ae5340ecf9ca755ce2c13327ef7d9aeeedb941
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-20 10:31:28 +0000

    [automatic commit] 2025-06-20 10:31:28 UTC 20.227.141.209

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version)
+// POST login 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   console.log("Login request received with:", email, password); // added console log to debug
