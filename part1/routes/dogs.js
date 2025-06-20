102cee5c27b1f28e5e5bf06826cfff6421b17dcb
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-20 07:14:52 +0000

    [automatic commit] 2025-06-20 07:14:52 UTC 20.227.141.211

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,17 +3,6 @@ const router = express.Router();
 const pool = require('../db');
 
 router.get('/', async (req, res) => {
-  const db = req.app.get('db');
-  try {
-    const [rows] = await db.query('SELECT ...');
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-
-router.get('/', async (req, res) => {
   try {
     const [rows] = await pool.query(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
