e3d5efa090db59d21b694d181eb0b2ca664bbaf7
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-20 07:20:33 +0000

    [automatic commit] 2025-06-20 07:20:33 UTC 20.227.141.211

--- part1/app.js
+++ part1/app.js
@@ -103,4 +103,9 @@ let db;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 module.exports = app;
