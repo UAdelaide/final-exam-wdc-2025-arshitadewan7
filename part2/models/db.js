d4750a183ac073606f15817cc242f601cbd1d512
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-20 09:52:21 +0000

    [automatic commit] 2025-06-20 09:52:21 UTC 20.227.141.209

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '',
   user: 'root',
   database: 'DogWalkService',
   waitForConnections: true,
