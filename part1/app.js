44989ab49ae98d98d8a5c521afdf71de9dd8f7ba
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-20 07:08:50 +0000

    [automatic commit] 2025-06-20 07:08:50 UTC 20.227.141.211

--- part1/app.js
+++ part1/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const mysql = require('mysql2/promise');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
