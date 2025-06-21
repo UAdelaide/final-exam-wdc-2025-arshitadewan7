33f9a54c0c5a629416b93cff616a923bd9e77f08
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-21 04:55:11 +0000

    [automatic commit] 2025-06-21 04:55:11 UTC 20.227.141.210

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session'); // added for express session
-const cookieParser = require('cook')
+const cookieParser = require('cookie-parser'); // added cookie parser
 require('dotenv').config();
 
 const app = express();
