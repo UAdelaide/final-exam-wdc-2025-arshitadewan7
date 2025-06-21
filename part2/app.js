559cac679bebc4820204a1d8815bfeabd51b89a1
arshitadewan7 <161274535+arshitadewan7@users.noreply.github.com>
2025-06-21 04:56:11 +0000

    [automatic commit] 2025-06-21 04:56:11 UTC 20.227.141.210

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(cookieParser())
+app.use(cookieParser()); // added cookie parser
 app.use(session({ // added functionality for express session
   secret: 'mykey',
   resave: false,
